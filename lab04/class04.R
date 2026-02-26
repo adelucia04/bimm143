0e9967587a352387540dae7cb94c648fbc3010d4
Anjolina DeLucia <anjolinadelucia@Anjolinas-MBP.attlocal.net>
2026-02-25 20:39:04 -0800

    Added Files from past classes

---
+++ lab04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Joli DeLucia"
+#' date: "January 16th, 2026"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x, sin(x), typ="l", col="pink", lwd=3, xlab="is it lunch time yet?")
